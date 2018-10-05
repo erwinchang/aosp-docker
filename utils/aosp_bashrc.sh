6c46cee1c57ece38f5c7890bdd470dd265031359
erwin.chang <m9207216@mail.ntust.edu.tw>
2018-10-05 11:53:37 +0800

    print cacche status

--- utils/aosp_bashrc.sh
+++ utils/aosp_bashrc.sh
@@ -6,10 +6,17 @@ cc_cmd=${WORK_DIR}/android_4_4_2/prebuilts/misc/linux-x86/ccache/ccache
 
 if [ "$USE_CCACHE" == "1" -a -f $cc_cmd ]; then
 	$cc_cmd -M 5G
+	echo ""
+	echo "android cache enabled success"
+	echo "cache patch CCACHE_DIR:$CCACHE_DIR"
 
 elif [ "$USE_CCACHE" == "1" -a ! -f $cc_cmd ]; then
-	echo "cacche enabled command: $cc_cmd -M 5G"
-
+	echo ""
+	echo "android cacche off"
+	echo "ex. $cc_cmd -M 5G"
+else
+	echo ""
+	echo "android cacche off"
 fi
 
 alias dbsp='cd ${WORK_DIR}'
